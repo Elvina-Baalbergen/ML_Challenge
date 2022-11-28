5ec698d8fb8ab652212796f7e817544573336370
Elvina-Baalbergen <elvina.baalbergen@gmail.com>
2022-11-28 19:28:24 +0100

    add ulviyya cleaning

--- source_code/features/2_processing_data.py
+++ source_code/features/2_processing_data.py
@@ -1,13 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 import os
-
-import re
-from textblob import Word
-from string import punctuation as pn
-from nltk.stem.snowball import SnowballStemmer
-from gensim.parsing.preprocessing import STOPWORDS
 
 #########
 # auth: Elvina
@@ -23,16 +23,33 @@ def encode_venues(df_train):
 
     return df_train_venue
 
-#########
-# auth: Ulviyya
-##########
+# Set working directory to location of the file
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+# Open raw data
+df_train = pd.read_pickle("../../data/processed/dirty_df.pkl")
+
+# Call cleaning functions
+df_train = encode_venues(df_train)
+
+# write back to processed folder
+df_train.to_pickle("../../data/processed/clean_df.pkl")
+
+#cleaning text rows
 def process_row(row):
+    import re
+    from textblob import Word
+    from string import punctuation
+    from nltk.stem.snowball import SnowballStemmer
+    from gensim.parsing.preprocessing import STOPWORDS
     #Mail address
     row = re.sub('(\S+@\S+)(com|\s+com)', ' ', row)
     #Username
     row = re.sub('(\S+@\S+)', ' ', row)
     #punctuation & Lower case
-    punctuation = pn + '\n' + '—“,”‘-’' #+ '0123456789'
+    punctuation = punctuation + '\n' + '—“,”‘-’' #+ '0123456789'
     row = ''.join(word.lower() for word in row if word not in punctuation)
     #Stopwords & Lemma
     stop = STOPWORDS
@@ -45,18 +62,3 @@ def process_row(row):
 
     return row
 
-
-# Set working directory to location of the file
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
-
-# Open raw data
-df_train = pd.read_pickle("../../data/processed/dirty_df.pkl")
-
-# Call cleaning functions
-df_train = encode_venues(df_train)
-df_train['abstract'] = df_train['abstract'].apply(process_row)
-
-# write back to processed folder
-df_train.to_pickle("../../data/processed/clean_df.pkl")
